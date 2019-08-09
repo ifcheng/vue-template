ea21578330449fa99ba3a3ba176b1269c8e6fa2a
ifcheng <fchengmail@163.com>
2019-08-09 15:37:09 +0800

    更新vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/foo/' : '/',
+  // 如果路由使用的是 history 模式，publicPath 应为绝对路径
+  publicPath: './',
   productionSourceMap: false,
+  lintOnSave: process.env.NODE_ENV !== 'production',
   devServer: {
+    open: true,
     proxy: {}
   }
 }

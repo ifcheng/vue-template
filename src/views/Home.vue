a1f21674bf128d6c632522736ca78899cc3eaac2
ifcheng <fchengmail@163.com>
2019-08-27 11:03:57 +0800

    增加'img'路径别名

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="home">
+  <div class="home" style="text-align: center">
+    <img src="~img/logo.png" alt="" />
     <h1>Welcome to Your Vue.js App</h1>
   </div>
 </template>

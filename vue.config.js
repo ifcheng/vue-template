05a5657d5e4492718459e2581ca9066cc7ca74b7
ifcheng <fchengmail@163.com>
2019-08-27 11:40:23 +0800

    添加‘img’路径别名

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   // 如果路由使用的是 history 模式，publicPath 应为绝对路径
   publicPath: './',
@@ -5,5 +7,12 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
   devServer: {
     open: true
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        img: path.join(__dirname, 'src/assets/img')
+      }
+    }
   }
 }
